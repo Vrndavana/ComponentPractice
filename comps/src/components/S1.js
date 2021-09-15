a37e5e1e0e5335a4fcf25b21bf231f9985acf881
unknown <vrndavana.th@gmail.com>
2021-09-14 21:23:11 -0700

    State

--- comps/src/components/S1.js
+++ comps/src/components/S1.js
@@ -1,27 +1,33 @@
-import React from 'react';
+import React, {useState} from "react";
 import {Link} from 'react-router-dom';
 
 
-function S1() {
 
-    return(
-
+const S1 = () => {
+const [value, setValue] = useState(1);
+    return (
         <div>
-           <nav>
-             <Link to='/' id="sasd">             HOME             </Link>
-             <Link to='/Projects' id="asd">      Projects         </Link>
-            </nav>
-
-
-
-
-
-
-
-            <nav>
-             <Link to='/S2' id="sasd">           Step 2           </Link>
-             <Link to='/S3' id="sasd">           Step 3           </Link>
-            </nav>
+        <nav>
+         <Link to='/' id="sasd">             HOME             </Link>
+         <Link to='/Projects' id="asd">      Projects         </Link>
+        </nav>
+        <div>
+        <p> This example shows how STATE, is saved and updated. Inscrea the number</p> <h3> {value} </h3> 
+        <button onClick={() => setValue((value + 1))}>Increment Value</button>
         </div>
-    )}
-    export default S1;
+        <nav>
+         <Link to='/S2' id="sasd">           Step 2           </Link>
+         <Link to='/S3' id="sasd">           Step 3           </Link>
+        </nav>
+
+
+        </div>
+    );
+};
+export default S1;
+
+
+
+
+
+    
